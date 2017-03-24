86b648a8ed17e6c100b0e976bc568fa8cdaa471f
Hasnain Khan <Hasnainkhankqs@gmail.com>
2017-03-24 10:18:35 +0500

    adding jquery learning

--- form/app.js
+++ form/app.js
@@ -1,16 +1,27 @@
 
-var nameptt = /^[a-zA-Z\s]+$/
-var emailpattrn =  /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$/
+var nameptt = /^[a-zA-Z\s]+$/;
+var emailpattrn =  /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$/;
+var nicpatt = /^[0-9]{,5}+-+[0-9]{,7}+-+[0-9]{,1}$/;
 
 var submit = document.getElementById("submit");
 
 
 submit.addEventListener("click",function(){
+var contactpatt = /[0-9]+-+[0-9]$/;
+var contact = document.getElementById("contact").value;
+if(contactpatt.test(contact)){
+    // alert("okay");
+}
+else{
+    // alert("somthing bad");
+}
 
-var contact = document.getElementById("contact");
-var contactVal = contact.value;
 var nic = document.getElementById("nic");
 var nicVal = nic.value;
+if(!nicpatt.test(nicVal)){
+   
+    alert("Please write correct Nic number")
+}
 var password = document.getElementById("password");
 var passVal = password.value;
 
@@ -18,19 +29,19 @@ var passVal = password.value;
 var fname = document.getElementById("fname");
 var fnameVal = fname.value;
 if(!nameptt.test(fnameVal)){
-    alert("write correct fathername")
+    // alert("write correct fathername")
 }
 // name validation
 var name = document.getElementById("name");
 var  nameVal = name.value;
 if(!nameptt.test(nameVal)){
-    alert("write correct name")
+    // alert("write correct name")
 }
 // email validation
 var email = document.getElementById("email");
 var emailVal = email.value;
 if(!emailpattrn.test(emailVal)){
-     alert("write correct email")
+    //  alert("write correct email")
 }
 
 //        if(!isNaN(nameVal) ||  nameVal == ""){
@@ -130,13 +141,13 @@ if(!emailpattrn.test(emailVal)){
 //         console.log("False")
 //     }
 
-var matchit =  /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$/
-    var a = "hasnainkhankqs@gmail.com";
-    if(matchit.test(a)) {
-        console.log("true");
-    }
-    else{
-        console.log("False")
-    }
+// var matchit =  /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$/
+//     var a = "hasnainkhankqs@gmail.com";
+//     if(matchit.test(a)) {
+//         console.log("true");
+//     }
+//     else{
+//         console.log("False")
+//     }
 // http://eloquentjavascript.net/09_regexp.html
 
