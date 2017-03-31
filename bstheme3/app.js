891f593d115e48810b5ee97cbe223cd4da2db78b
Hasnain Khan <Hasnainkhankqs@gmail.com>
2017-03-31 17:01:13 +0500

    adding new row to theme 3

--- bstheme3/app.js
+++ bstheme3/app.js
@@ -1,10 +1,15 @@
 
 $(window).scroll(function(){
 if($(this).scrollTop() > "300"){
-    $(".navbar-fixed-top").css({"background-color":"#fff","transition":"0.5s"})
+    $(".navbar-fixed-top").css({"background-color":"rgba(227, 174, 87, 0.45)","transition":"0.5s"});
+    $(".navbar-fixed-top a").css({"color":"white"});
+    // $(".navbar-fixed-top").css({"box-shadow":"8px 8px 12px rgba(0,0,0,0.5)"});
+
 }
 else{
-    $(".navbar-fixed-top").css({"background-color":"transparent","transition":"0.5s"})
+    $(".navbar-fixed-top").css({"background-color":"transparent","transition":"0.5s"});
+     $(".navbar-fixed-top a").css({"color":"#777"})
+    //  $(".navbar-fixed-top").css({"box-shadow":"none"});
 }
 })
 // animation on text
