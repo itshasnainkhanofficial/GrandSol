261dcdcd26744cd5912f4c1bc323f9ce2959fe99
Hasnain Khan <Hasnainkhankqs@gmail.com>
2017-04-05 21:42:30 +0500

    50%

--- Banquet2/app.js
+++ Banquet2/app.js
@@ -20,30 +20,30 @@ for(var a = 0 ; a < openmenu.length ; a++){
 
 
 // slider animation start
-// var img = document.getElementById("slider").style;
-// var count = 1;
-// function slidering(){
-//     count++;
-//     if(count > 4){
-//         count = 1;
-//     }
-//     img.backgroundImage = "url(img/slider"+count+".jpg"+")";
-//     if(count === 1){
-//         document.getElementById("changeabletxt").innerHTML = "<span>We Create</span><br/> You<br/> Celebrate"
-//     }
-//         if(count === 2){
-//         document.getElementById("changeabletxt").innerHTML = "<span>We Make</span><br/> You<br/> Special"
-//     }
-//         if(count === 3){
-//         document.getElementById("changeabletxt").innerHTML = "<span>We Relize</span><br/> You <br/> Comfortable"
-//     }
-//         if(count === 4){
-//         document.getElementById("changeabletxt").innerHTML = "<span>We Wish</span><br/> You<br/> Best"
-//     }
+var img = document.getElementById("slider").style;
+var count = 1;
+function slidering(){
+    count++;
+    if(count > 4){
+        count = 1;
+    }
+    img.backgroundImage = "url(img/slider"+count+".jpg"+")";
+    if(count === 1){
+        document.getElementById("changeabletxt").innerHTML = "<span>We Create</span><br/> You<br/> Celebrate"
+    }
+        if(count === 2){
+        document.getElementById("changeabletxt").innerHTML = "<span>We Make</span><br/> You<br/> Special"
+    }
+        if(count === 3){
+        document.getElementById("changeabletxt").innerHTML = "<span>We Relize</span><br/> You <br/> Comfortable"
+    }
+        if(count === 4){
+        document.getElementById("changeabletxt").innerHTML = "<span>We Wish</span><br/> You<br/> Best"
+    }
 
-//     console.log("image changed");
-// }
-// setInterval("slidering()",10000);
+    console.log("image changed");
+}
+setInterval("slidering()",10000);
 
 // map start
 // function initMap() {
